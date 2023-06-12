373046cf31510fbe7aa51906b75a9783326b652d
Alexander Ehrlich <aehrlich@c4b10c2.42berlin.de>
2023-06-12 15:51:12 +0200

    pipes set up

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aehrlich <aehrlich@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: aehrlich <aehrlich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 14:58:03 by aehrlich          #+#    #+#             */
-/*   Updated: 2023/06/11 20:30:54 by aehrlich         ###   ########.fr       */
+/*   Updated: 2023/06/12 15:46:01 by aehrlich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,15 +37,56 @@ int	**ft_create_pipes(t_list *commands)
 	return (pipes);
 }
 
+int	io_redirection(t_list *command, int **pipes, int index)
+{
+	t_command *casted_cmd;
+
+	casted_cmd = (t_command *)command->content;
+	if (casted_cmd->has_in_pipe)
+		dup2(pipes[index - 1][0], STDIN_FILENO);
+	if (casted_cmd->has_out_pipe)
+		dup2(pipes[index][1], STDOUT_FILENO);
+	return (0);
+}
+
+int	children(int **pipes, int *pids, t_list *commands)
+{
+	int	i;
+
+	i = 0;
+	while (commands)
+	{
+		pids[i] = fork();
+		if (pids[i] == -1)
+			return (-1);
+		if (pids[i] == 0)
+		{
+			char *temp[] = {"cat", "file1", NULL};
+			io_redirection(commands, pipes, i);
+			if (execve("/usr/bin/cat", temp, NULL) == -1)
+				perror("execve");
+		}
+		commands = commands->next;
+		i++;
+	}
+	return (0);
+}
+
 int	execute(t_list *commands)
 {
 	int	**pipes;
 	int	*pids;
 	int	i;
+	int	cmd_count;
 	
 	i = 0;
+	cmd_count = ft_lstsize(commands);
 	pipes = ft_create_pipes(commands);
+	pids = malloc(cmd_count * sizeof(int));
 	if (!pipes)
 		return (printf("Error creating pipes\n"), 1);
+	children(pipes, pids, commands);
+	while (i < cmd_count)
+		waitpid(pids[i++], NULL, 0);
 	return (0);
 }
--- srcs/executor/executor_utils.h
+++ srcs/executor/executor_utils.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executor_utils.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aehrlich <aehrlich@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: aehrlich <aehrlich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 14:24:32 by aehrlich          #+#    #+#             */
-/*   Updated: 2023/06/10 15:24:55 by aehrlich         ###   ########.fr       */
+/*   Updated: 2023/06/12 09:48:01 by aehrlich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define EXECUTOR_UTILS_H
 
 # include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
 # include "../../libft/includes/libft.h"
 # include "../../includes/types.h"
 
