429886a83788c5e7b620b91b7d32cabaa1ae0208
Leon <leon.yannik@web.de>
2023-06-19 15:24:20 +0200

    save progress again to work remote

--- srcs/builtins/builtin_utils.h
+++ srcs/builtins/builtin_utils.h
@@ -6,7 +6,7 @@
 /*   By: lbaumann <lbaumann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 12:56:13 by lbaumann          #+#    #+#             */
-/*   Updated: 2023/06/19 12:43:07 by lbaumann         ###   ########.fr       */
+/*   Updated: 2023/06/19 15:12:12 by lbaumann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@
 # include <stdio.h>
 # include <stdlib.h> //getenv
 # include <limits.h>
-#include "../../libft/includes/libft.h"
+# include "../../libft/includes/libft.h"
+# include "../../includes/error.h"
+# include "../../includes/builtins.h"
 
 
 
--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,19 +6,28 @@
 /*   By: lbaumann <lbaumann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 15:13:03 by lbaumann          #+#    #+#             */
-/*   Updated: 2023/06/19 13:10:31 by lbaumann         ###   ########.fr       */
+/*   Updated: 2023/06/19 15:24:04 by lbaumann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin_utils.h"
 
+/**
+ * free new_path because it has been allocated before
+*/
 static void	update_paths(char *new_path, t_list *dict)
 {
-	char	*pwd;
+	char	cwd[PATH_MAX];
 
-	pwd = ft_dict_get_value(dict, "PWD");
-	ft_dict_modify_value(dict, "PWD", new_path);
-	ft_dict_modify_value(dict, "OLDPWD", pwd);
+	if (!getcwd(cwd, PATH_MAX))
+		perror("cd");
+	ft_dict_modify_value(dict, "OLDPWD", cwd);
+	if (!chdir(new_path))
+		perror("cd");
+	if (!getcwd(cwd, PATH_MAX))
+		perror("cd");
+	ft_dict_modify_value(dict, "PWD", cwd);
+	new_path = ft_free_set_null(new_path);
 }
 
 void	error_cd(const char *msg)
@@ -34,14 +43,26 @@ static char	*skip_fwdslashes(char *path)
 }
 
 /**
- * if arg starts with . or .. -> 
+ * cd [directory]
+ * 
+ * [directory] starts with:
+ * . or .. -> set curpath to the [directory] operand
+ * '/' -> absolute path: curpath = [directory]
+ * no slash -> relative path: strjoin PWD and [directory]
 */
-char	*process_path(const char *arg)
+char	*process_path(const char *arg, t_list *dict)
 {
+	char	*pwd;
 	char	*curpath;
 
-	curpath = ft_strdup(arg);
-PATH
+	if (!ft_strncmp(arg, ".", 1) || !ft_strncmp(arg, "..", 2) || !ft_strncmp(arg, "/", 1))
+		curpath = ft_strdup(arg);
+	else
+	{
+		pwd = ft_dict_get_value(dict, "PWD");
+		curpath = ft_strjoin(pwd, arg);
+	}
+	return (curpath);
 }
 
 void	builtin_cd(const char **argv, t_list *dict)
@@ -54,12 +75,13 @@ void	builtin_cd(const char **argv, t_list *dict)
 	else if (!argv[1] || !ft_strcmp(argv[1], "~") || !ft_strcmp(argv[1], "--"))
 		path = ft_dict_get_node(dict, "HOME");
 	else if (!ft_strcmp(argv[1], "-"))
-		path = ft_dict_get_node(dict, "OLDPWD");
-	else
-		path = process_path(argv[1]);
-	if (path)
 	{
+		path = ft_dict_get_node(dict, "OLDPWD");
 		update_paths(path, dict);
-		chdir(path);
+		return (builtin_pwd());
 	}
+	else
+		path = process_path(argv[1], dict);
+	if (path)
+		update_paths(path, dict);
 }
